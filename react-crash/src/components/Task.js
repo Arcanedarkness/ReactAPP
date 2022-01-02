01b9dce37baec5fcbb931280d2a45bb0d9b4859a
Niklas Jørgensen <niklas220587@hotmail.com>
2022-01-02 14:46:02 +0100

    comment

--- react-crash/src/components/Task.js
+++ react-crash/src/components/Task.js
@@ -2,6 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
+        // wierd syntax he used : {`task ${task.reminder ? 'reminder' : ''}`}
         <div className={ task.reminder ? 'task reminder' : 'task'} onDoubleClick={() => onToggle(task.id)}>
             <h3>
                 {task.text}{' '}
