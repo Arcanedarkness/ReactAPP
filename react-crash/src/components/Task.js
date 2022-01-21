12994d1ec67a22e2a821e6135f112e24b3278662
Niklas Jørgensen <niklas220587@hotmail.com>
2022-01-21 12:24:53 +0100

    removed wierd comment

--- react-crash/src/components/Task.js
+++ react-crash/src/components/Task.js
@@ -2,7 +2,6 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        // wierd syntax he used : {`task ${task.reminder ? 'reminder' : ''}`}
         <div className={ task.reminder ? 'task reminder' : 'task'} onDoubleClick={() => onToggle(task.id)}>
             <h3>
                 {task.text}{' '}
